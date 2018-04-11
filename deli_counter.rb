6dfae9c3bd4cc608b0be5a16b1331e35fd1c3dab
Richard Cho <richardcho100@gmail.com>
2018-04-11 21:46:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,13 @@ def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
   elsif deli.size > 0
+    new_arr = []
     deli.each_with_index.collect do |value, index|
       binding.pry
       "#{index + 1}. #{value}"
-    #  number = i + 1
     # binding.pry
     #  puts "The line is currently: " + "#{number}. #{customer} #{number}. #{customer}"
     end
+
   end
 end
